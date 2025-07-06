eea47eedc96d0b421b94c6b41ffc0642cad37863
HugoRCD <hugo.richard@epitech.eu>
2025-07-06 20:28:34 +0200

    up

--- app/components/CommandConsole.vue
+++ app/components/CommandConsole.vue
@@ -64,13 +64,22 @@ defineShortcuts({
         :ui="commandPaletteUi" 
         :fuse="{ resultLimit: 100, fuseOptions: { includeMatches: true } }"
         @highlight="handleHighlight"
-        @select="handleSelect"
       >
         <template #item="{ item }">
-          <CommandItem 
-            :item 
-            @right-click="handleRightClick" 
-          />
+          <div class="flex items-center justify-between w-full p-1" @contextmenu="handleRightClick">
+            <div class="flex items-center gap-2.5">
+              <div class="flex items-center gap-2.5">
+                <NuxtImg :src="item.icon!" class="size-5" />
+                <span>{{ item.label }}</span>
+              </div>
+              <span class="text-muted font-medium">
+                {{ item.suffix }}
+              </span>
+            </div>
+            <span class="text-muted text-xs font-medium">
+              {{ item.type }}
+            </span>
+          </div>
         </template>
 
         <template #close>
